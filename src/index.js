d7739c87e78dc8dc63f01d005fbf0529623187d9
merior nordway <meriornordway@yandex.ru>
2022-04-30 01:37:11 +0300

    another freakin slider hate this

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import modal from './modules/modal'
 import three from './modules/threeslider'
 import swipeUp from './modules/swipeUp'
+import twoSloder from './modules/twoSlider'
 
 modal()
 three()
 swipeUp()
+twoSloder()
